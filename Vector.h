9bbaf4b604fd8ca23d85053f8d5c10c167d0a3ea
radu1902 <radu.pultea@gmail.com>
2024-10-10 00:04:30 +0300

    vector pushback

---
+++ Vector.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <stdlib.h>
+
+struct Vector
+{
+	float* elements;
+	unsigned int size;
+	unsigned int capacity;
+};
+
+struct Vector vectorInit()
+{
+    struct Vector v;
+    v.size = 0;
+    v.capacity = 1;
+    v.elements = (float*)malloc(sizeof(float));
+    return v;
+}
+
+void push_back(struct Vector* vec, float elem)
+{
+    vec->size++;
+    if (vec->size >= vec->capacity)
+        resize(vec);
+
+    vec->elements[vec->size - 1] = elem;
+}
+
+void resize(struct Vector* vec)
+{
+    float* tempElements = (float*)malloc(sizeof(float) * vec->size);
+    for (unsigned int i = 0; i < vec->size; i++)
+    {
+        tempElements[i] = vec->elements[i];
+    }
+    free(vec->elements);
+
+    vec->capacity = vec->capacity * 2;
+    vec->elements = (float*)malloc(sizeof(float) * vec->capacity);
+    for (unsigned int i = 0; i < vec->size; i++)
+    {
+        vec->elements[i] = tempElements[i];
+    }
+    free(tempElements);
+}
--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "Vector.h"
 
-void main()
+int main()
 {
 
+
+    return 0;
 }
