d85dbd3fab2dbfa5f59aae38cafb6d2c43303eea
radu1902 <radu.pultea@gmail.com>
2024-10-10 10:48:03 +0300

    Menu start

--- Vector.h
+++ Vector.h
@@ -8,38 +8,6 @@ struct Vector
 	unsigned int capacity;
 };
 
-struct Vector vectorInit()
-{
-    struct Vector v;
-    v.size = 0;
-    v.capacity = 1;
-    v.elements = (float*)malloc(sizeof(float));
-    return v;
-}
-
-void push_back(struct Vector* vec, float elem)
-{
-    vec->size++;
-    if (vec->size >= vec->capacity)
-        resize(vec);
-
-    vec->elements[vec->size - 1] = elem;
-}
-
-void resize(struct Vector* vec)
-{
-    float* tempElements = (float*)malloc(sizeof(float) * vec->size);
-    for (unsigned int i = 0; i < vec->size; i++)
-    {
-        tempElements[i] = vec->elements[i];
-    }
-    free(vec->elements);
-
-    vec->capacity = vec->capacity * 2;
-    vec->elements = (float*)malloc(sizeof(float) * vec->capacity);
-    for (unsigned int i = 0; i < vec->size; i++)
-    {
-        vec->elements[i] = tempElements[i];
-    }
-    free(tempElements);
-}
+struct Vector vectorInit();
+void push_back(struct Vector* vec, float elem);
+void resize(struct Vector* vec);
--- main.c
+++ main.c
@@ -4,6 +4,37 @@
 
 int main()
 {
+    int dataStructureChoice = 0;
+
+    printf("Choose data structure to work with:\n");
+    printf("1.Vector\n");
+    printf("2.Stack\n");
+    printf("3.Queue\n");
+    printf("4.Doubly linked list\n");
+    printf("5.Hashmap\n");
+    printf("6.Heap\n");
+    printf("7.Binary tree\n");
+    printf("8.Binary search tree\n");
+    printf("9.Self-balancing search tree\n");
+    printf("0.exit\n");
+
+    scanf("%d", &dataStructureChoice);
+
+    struct Vector v;
+
+    int operationChoice = 0;
+    while (dataStructureChoice != 0)
+    {
+        if (dataStructureChoice == 1)
+        {
+            printf("Choose vector operation:");
+            printf("1.Pushback element");
+            printf("1.Pop element");
+            printf("1.Read (Specify number of values, followed by the values themselves)");
+            printf("1.Print (Show entire vector data)");
+            printf("1.Print");
+        }
+    }
 
 
     return 0;
